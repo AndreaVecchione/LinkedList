5df34594678fcd7e117fd81074924b5366bc43a0
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-06-10 10:21:30 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,26 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //creo degli oggetti che andrò poi a mettere all'interno della lista
 Fruit fruit1 = new Fruit ("Mela");
 Fruit fruit2 = new Fruit ("Arancia");
 Fruit fruit3 = new Fruit ("Fragola");
 Fruit fruit4 = new Fruit ("Lamponi");
 
+//Creo la lista Linkedlist dove andrò a inserirmi i primi oggetti
+
 LinkedList<Fruit> list = new LinkedList<>();
 list.add(fruit1);
 list.add(fruit2);
-System.out.println("list is: " + list);
+System.out.println("lista e: " + list);
+
+//aggiorno la lista, aggiungendo al primo e all'ultimo posto altri oggetti
 
 list.addFirst(fruit4);
 list.addLast(fruit3);
-System.out.println("list updated is: " + list);
+System.out.println("list aggiornata e: " + list);
 
     }
 }
